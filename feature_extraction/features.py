fcb6b4389ba6fb8569c6f98211dd480cc5937c8e
veronika_levasheva <v.levasheva@innopolis.university>
2026-07-19 20:10:39 +0300

    feat[f-extraction]: implement feature tensors creation

--- feature_extraction/features.py
+++ feature_extraction/features.py
@@ -7,16 +7,49 @@ freq_bands = {
     "theta": [4, 8],
     "alpha": [8, 13],
     "beta": [13, 30],
-    "gamma": [30, 100],}
+    "gamma": [30, 100],
+}
 
-def get_raw_tensor(windows : mne.Epochs) -> torch.Tensor:
-    tensor = windows.get_data(copy=True) # (n_epochs, n_channels, n_times)
+
+def get_raw_tensor(windows: mne.Epochs) -> torch.Tensor:
+    tensor = windows.get_data(copy=True)  # (n_epochs, n_channels, n_times)
     return torch.tensor(tensor)
 
-def get_spectral_tensor(windows : mne.Epochs) -> torch.Tensor:
+
+def get_feature_tensor(windows: mne.Epochs) -> torch.Tensor:
     data = windows.get_data(copy=True)
     freq = windows.info["sfreq"]
-    tensor = mne_features.feature_extraction.extract_features(X=data, sfreq=freq, selected_funcs=["pow_freq_bands"],
-                                                      funcs_params={"pow_freq_bands__freq_bands" : freq_bands,
-                                                                     "pow_freq_bands__freq_bands" : True})
+    power_tensor = mne_features.feature_extraction.extract_features(
+        X=data,
+        sfreq=freq,
+        selected_funcs=["pow_freq_bands"],
+        funcs_params={
+            "pow_freq_bands__freq_bands": freq_bands,
+            "pow_freq_bands__normalize": True,
+        },
+    )
+    fractal_dim_tensor = mne_features.feature_extraction.extract_features(
+        X=data, sfreq=freq, selected_funcs=["higuchi_fd"]
+    )
+    entropy_tensor = mne_features.feature_extraction.extract_features(
+        X=data,
+        sfreq=freq,
+        selected_funcs=["spect_entropy"],
+        funcs_params={"spect_entropy__psd_method": "fft"},
+    )
+    wavelet_tensor = mne_features.feature_extraction.extract_features(
+        X=data, sfreq=freq, selected_funcs=["wavelet_coef_energy"]
+    )
+    print("Feature tensors shapes:")
+    print("Power Spectrum: ", power_tensor.shape)
+    print("Higuchi Fractal Dimension: ", fractal_dim_tensor.shape)
+    print("Spectral Entropy: ", entropy_tensor.shape)
+    print("Energy of Wavelet decomposition coefficients: ", wavelet_tensor.shape)
+    power_tensor = torch.from_numpy(power_tensor)
+    fractal_dim_tensor = torch.from_numpy(fractal_dim_tensor)
+    entropy_tensor = torch.from_numpy(entropy_tensor)
+    wavelet_tensor = torch.from_numpy(wavelet_tensor)
+    tensor = torch.cat(
+        [power_tensor, fractal_dim_tensor, entropy_tensor, wavelet_tensor], dim=1
+    )
     return torch.tensor(tensor)
